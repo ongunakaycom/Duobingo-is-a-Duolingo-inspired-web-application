827c909df766a1f4275f9dde9c6c94a7e9207e9e
Ongun <info@ongunakay.com>
2024-12-20 21:51:27 +0300

    Add language selection feature and landing components; include styles for language selection

--- src/components/LandingFeatures.vue
+++ src/components/LandingFeatures.vue
@@ -1,49 +1,43 @@
 <template>
-    <div class="features">
-      <h2>Why Choose Duolingo?</h2>
-      <div class="feature-list">
-        <div class="feature-item">
-          <h3>Interactive Lessons</h3>
-          <p>Enjoy engaging and interactive language lessons.</p>
-        </div>
-        <div class="feature-item">
-          <h3>Variety of Languages</h3>
-          <p>Choose from a wide range of languages to learn.</p>
-        </div>
-        <div class="feature-item">
-          <h3>Track Your Progress</h3>
-          <p>See your progress and level up as you go.</p>
-        </div>
+  <div class="features">
+    <h2>Why Choose Duolingo?</h2>
+    <div class="feature-list">
+      <div class="feature-item">
+        <h3>Interactive Lessons</h3>
+        <p>Enjoy engaging and interactive language lessons.</p>
+      </div>
+      <div class="feature-item">
+        <h3>Variety of Languages</h3>
+        <p>Choose from a wide range of languages to learn.</p>
+      </div>
+      <div class="feature-item">
+        <h3>Track Your Progress</h3>
+        <p>See your progress and level up as you go.</p>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'LandingFeatures'
-  };
-  </script>
-  
-  <style scoped>
-  .features {
-    margin-top: 40px;
-  }
-  
-  .feature-list {
-    display: flex;
-    justify-content: space-around;
-  }
-  
-  .feature-item {
-    width: 30%;
-  }
-  
-  .feature-item h3 {
-    font-size: 1.5em;
-  }
-  
-  .feature-item p {
-    font-size: 1em;
-  }
-  </style>
-  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingFeatures'
+};
+</script>
+
+<style scoped>
+.features {
+  text-align: center;
+  padding: 20px;
+}
+
+.feature-list {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+
+.feature-item {
+  max-width: 300px;
+  text-align: left;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,23 @@
 <template>
-    <div class="home">
-      <div class="jumbotron">
-        <h1 class="display-4">Welcome to Duolingo</h1>
-        <p class="lead">Learn a new language with fun and interactive lessons.</p>
-        <hr class="my-4">
-        <p>Choose from a variety of languages and start your journey today!</p>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Home'
-  };
-  </script>
-  
-  <style scoped>
-  /* Add any custom CSS styles here  */
-  .home {
-    text-align: center;
+  <div class="home">
+    <LandingFeatures />
+  </div>
+</template>
+
+<script>
+import LandingFeatures from '@/components/LandingFeatures.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    LandingFeatures
   }
-  
-  .jumbotron {
-    margin-top: 50px;
-    padding: 50px;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+/* Add any custom CSS styles here */
+.home {
+  text-align: center;
+}
+</style>
