f5ce7d1d7fb432d3e74968d7a62f1d15914f4ad2
Ongun <info@ongunakay.com>
2024-12-20 20:56:27 +0300

    //

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,14 @@ const webpack = require('webpack');
 
 module.exports = {
   configureWebpack: {
+    resolve: {
+      alias: {
+        process: 'process/browser',  // Polyfill for process
+      },
+    },
     plugins: [
       new webpack.ProvidePlugin({
-        process: 'process/browser',
+        process: 'process/browser',  // Provide process polyfill for usage
       }),
     ],
   },
