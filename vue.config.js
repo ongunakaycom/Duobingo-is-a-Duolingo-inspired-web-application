b32016297ae8bad3949ef03e6027f3d3965ef08d
Ongun <info@ongunakay.com>
2025-02-27 22:29:30 +0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -13,4 +13,16 @@ module.exports = {
       }),
     ],
   },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://duolingo-vue-backend.vercel.app', // Your backend URL
+        changeOrigin: true,  // Ensures the request origin is changed to the target URL
+        secure: false,       // Disable SSL verification (if needed for local dev)
+        pathRewrite: {
+          '^/api': '',  // Removes the '/api' prefix from the request path
+        },
+      },
+    },
+  },
 };
