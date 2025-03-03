fd28cf0ec646558ee926959eb34558cf9b2d3847
Ongun <info@ongunakay.com>
2025-03-04 01:00:58 +0300

    front-end color update

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,22 +1,22 @@
-<template>
-  <footer class="footer mt-auto py-4 bg-success">
-    <div class="container-fluid text-light">
+<template> 
+  <footer class="footer mt-auto py-4" style="background-color: #58cc02;">
+    <div class="container-fluid text-white">
       <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4">
         
         <!-- About Us -->
         <div class="col">
-          <h5 class="fw-bold">About us</h5>
+          <h5 class="fw-bold">About Us</h5>
           <ul class="list-unstyled">
-            <li><a href="#" class="text-light text-decoration-none">Courses</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Mission</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Approach</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Efficacy</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Handbook</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Research</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Careers</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Store</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Investors</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Contact us</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Courses</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Mission</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Approach</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Efficacy</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Handbook</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Research</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Careers</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Store</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Investors</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Contact Us</a></li>
           </ul>
         </div>
 
@@ -24,11 +24,11 @@
         <div class="col">
           <h5 class="fw-bold">Products</h5>
           <ul class="list-unstyled">
-            <li><a href="#" class="text-light text-decoration-none">Duolingo</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Duolingo for Schools</a></li>
-            <li><a href="#" class="text-light text-decoration-none">English Test</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Duolingo Math</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Podcast</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Lingo</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Lingo for Schools</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Lingo English Test</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Lingo Math</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Podcast</a></li>
           </ul>
         </div>
 
@@ -36,8 +36,8 @@
         <div class="col">
           <h5 class="fw-bold">Apps</h5>
           <ul class="list-unstyled">
-            <li><a href="#" class="text-light text-decoration-none">Android</a></li>
-            <li><a href="#" class="text-light text-decoration-none">iOS</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Android</a></li>
+            <li><a href="#" class="text-white text-decoration-none">iOS</a></li>
           </ul>
         </div>
 
@@ -45,10 +45,10 @@
         <div class="col">
           <h5 class="fw-bold">Help & Support</h5>
           <ul class="list-unstyled">
-            <li><a href="#" class="text-light text-decoration-none">FAQs</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Schools FAQs</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Test FAQs</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Status</a></li>
+            <li><a href="#" class="text-white text-decoration-none">FAQs</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Schools FAQs</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Test FAQs</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Status</a></li>
           </ul>
         </div>
 
@@ -56,17 +56,17 @@
         <div class="col">
           <h5 class="fw-bold">Privacy & Terms</h5>
           <ul class="list-unstyled">
-            <li><a href="#" class="text-light text-decoration-none">Community Guidelines</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Terms</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Privacy</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Community Guidelines</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Terms</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Privacy</a></li>
           </ul>
 
           <h5 class="fw-bold mt-3">Follow Us</h5>
-          <ul class="list-unstyled">
-            <li><a href="#" class="text-light text-decoration-none">Instagram</a></li>
-            <li><a href="#" class="text-light text-decoration-none">TikTok</a></li>
-            <li><a href="#" class="text-light text-decoration-none">Twitter</a></li>
-            <li><a href="#" class="text-light text-decoration-none">YouTube</a></li>
+          <ul class="list-unstyled d-flex gap-2">
+            <li><a href="#" class="text-white text-decoration-none">Instagram</a></li>
+            <li><a href="#" class="text-white text-decoration-none">TikTok</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Twitter</a></li>
+            <li><a href="#" class="text-white text-decoration-none">YouTube</a></li>
           </ul>
         </div>
 
@@ -80,7 +80,3 @@ export default {
   name: 'FooterComponent',
 };
 </script>
-
-<style scoped>
-/* Custom styles if needed */
-</style>
