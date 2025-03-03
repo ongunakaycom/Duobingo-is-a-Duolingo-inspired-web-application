0873287aeac262ea4e88a9af054c32ec21b44c9a
Ongun <info@ongunakay.com>
2025-03-04 00:46:56 +0300

    Front-end mobile footer updated

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,80 +1,75 @@
 <template>
-  <footer class="footer mt-auto py-3" style="background-color: rgb(88, 204, 2);">
-    <div class="container" style="max-width: 1024px;">
-      <div class="row">
-        <!-- Column 1: About Us -->
+  <footer class="footer mt-auto py-4 bg-success">
+    <div class="container-fluid text-light">
+      <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4">
+        
+        <!-- About Us -->
         <div class="col">
-          <h5 style="font-size: 19px; font-weight: 700; color: rgb(165, 237, 110);">About us</h5>
+          <h5 class="fw-bold">About us</h5>
           <ul class="list-unstyled">
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Courses</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Mission</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Approach</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Efficacy</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Duolingo Handbook</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Research</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Careers</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Brand guidelines</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Store</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Press</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Investors</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Contact us</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Courses</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Mission</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Approach</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Efficacy</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Handbook</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Research</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Careers</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Store</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Investors</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Contact us</a></li>
           </ul>
         </div>
 
-        <!-- Column 2: Products -->
+        <!-- Products -->
         <div class="col">
-          <h5 style="font-size: 19px; font-weight: 700; color: rgb(165, 237, 110);">Products</h5>
+          <h5 class="fw-bold">Products</h5>
           <ul class="list-unstyled">
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Duolingo</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Duolingo for Schools</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Duolingo English Test</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Duolingo Math</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Podcast</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Duolingo for Business</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Super Duolingo</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Gift Super Duolingo</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Duolingo Max</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Duolingo</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Duolingo for Schools</a></li>
+            <li><a href="#" class="text-light text-decoration-none">English Test</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Duolingo Math</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Podcast</a></li>
           </ul>
         </div>
 
-        <!-- Column 3: Apps -->
+        <!-- Apps -->
         <div class="col">
-          <h5 style="font-size: 19px; font-weight: 700; color: rgb(165, 237, 110);">Apps</h5>
+          <h5 class="fw-bold">Apps</h5>
           <ul class="list-unstyled">
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Duolingo for Android</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Duolingo for iOS</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Android</a></li>
+            <li><a href="#" class="text-light text-decoration-none">iOS</a></li>
           </ul>
         </div>
 
-        <!-- Column 4: Help and Support -->
+        <!-- Help and Support -->
         <div class="col">
-          <h5 style="font-size: 19px; font-weight: 700; color: rgb(165, 237, 110);">Help and support</h5>
+          <h5 class="fw-bold">Help & Support</h5>
           <ul class="list-unstyled">
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Duolingo FAQs</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Schools FAQs</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Duolingo English Test FAQs</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Status</a></li>
+            <li><a href="#" class="text-light text-decoration-none">FAQs</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Schools FAQs</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Test FAQs</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Status</a></li>
           </ul>
         </div>
 
-        <!-- Column 5: Privacy and Terms + Social -->
+        <!-- Privacy & Social -->
         <div class="col">
-          <h5 style="font-size: 19px; font-weight: 700; color: rgb(165, 237, 110);">Privacy and terms</h5>
+          <h5 class="fw-bold">Privacy & Terms</h5>
           <ul class="list-unstyled">
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Community guidelines</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Terms</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Privacy</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Respecting your "do not sell my personal information" rights</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Community Guidelines</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Terms</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Privacy</a></li>
           </ul>
-          <h5 style="font-size: 19px; font-weight: 700; color: rgb(165, 237, 110); margin-top: 20px;">Social</h5>
+
+          <h5 class="fw-bold mt-3">Follow Us</h5>
           <ul class="list-unstyled">
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Blog</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Instagram</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">TikTok</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">Twitter</a></li>
-            <li><a href="#" style="color: rgb(165, 237, 110); font-size: 15px; line-height: 22px;">YouTube</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Instagram</a></li>
+            <li><a href="#" class="text-light text-decoration-none">TikTok</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Twitter</a></li>
+            <li><a href="#" class="text-light text-decoration-none">YouTube</a></li>
           </ul>
         </div>
+
       </div>
     </div>
   </footer>
@@ -87,5 +82,5 @@ export default {
 </script>
 
 <style scoped>
-/* Add any additional custom styles here */
+/* Custom styles if needed */
 </style>
