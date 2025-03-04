3191bfe59b6e2f15c3c060252e131298c412d170
Ongun <info@ongunakay.com>
2025-03-04 17:27:51 +0300

    Front-end footer UI mobile responsive updated

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,49 +1,51 @@
 <template>
-  <footer class="footer mt-auto py-4" style="background-color: #58cc02;">
-    <div class="container"> <!-- Added container to align with other sections -->
-      <div class="row">
+  <div class="footer-container"> 
+    <div class="container"> <!-- Primary container wrapping all content securely -->
+      <footer class="footer mt-auto py-4">
+        <div class="row">
         
-        <div class="col-md-3 col-sm-6 footer-section">
-          <h5 class="fw-bold">About Us</h5>
-          <ul class="list-unstyled">
-            <li><a href="#" class="text-white text-decoration-none">Courses</a></li>
-            <li><a href="#" class="text-white text-decoration-none">Mission</a></li>
-            <li><a href="#" class="text-white text-decoration-none">Approach</a></li>
-            <li><a href="#" class="text-white text-decoration-none">Careers</a></li>
-          </ul>
-        </div>
+          <div class="col-md-3 col-sm-6 footer-section">
+            <h5 class="fw-bold">About Us</h5>
+            <ul class="list-unstyled">
+              <li><a href="#" class="text-white text-decoration-none">Courses</a></li>
+              <li><a href="#" class="text-white text-decoration-none">Mission</a></li>
+              <li><a href="#" class="text-white text-decoration-none">Approach</a></li>
+              <li><a href="#" class="text-white text-decoration-none">Careers</a></li>
+            </ul>
+          </div>
 
-        <div class="col-md-3 col-sm-6 footer-section">
-          <h5 class="fw-bold">Products</h5>
-          <ul class="list-unstyled">
-            <li><a href="#" class="text-white text-decoration-none">Lingo</a></li>
-            <li><a href="#" class="text-white text-decoration-none">Lingo Math</a></li>
-            <li><a href="#" class="text-white text-decoration-none">Podcast</a></li>
-          </ul>
-        </div>
+          <div class="col-md-3 col-sm-6 footer-section">
+            <h5 class="fw-bold">Products</h5>
+            <ul class="list-unstyled">
+              <li><a href="#" class="text-white text-decoration-none">Lingo</a></li>
+              <li><a href="#" class="text-white text-decoration-none">Lingo Math</a></li>
+              <li><a href="#" class="text-white text-decoration-none">Podcast</a></li>
+            </ul>
+          </div>
 
-        <div class="col-md-3 col-sm-6 footer-section">
-          <h5 class="fw-bold">Help & Support</h5>
-          <ul class="list-unstyled">
-            <li><a href="#" class="text-white text-decoration-none">FAQs</a></li>
-            <li><a href="#" class="text-white text-decoration-none">Test FAQs</a></li>
-            <li><a href="#" class="text-white text-decoration-none">Status</a></li>
-          </ul>
-        </div>
+          <div class="col-md-3 col-sm-6 footer-section">
+            <h5 class="fw-bold">Help & Support</h5>
+            <ul class="list-unstyled">
+              <li><a href="#" class="text-white text-decoration-none">FAQs</a></li>
+              <li><a href="#" class="text-white text-decoration-none">Test FAQs</a></li>
+              <li><a href="#" class="text-white text-decoration-none">Status</a></li>
+            </ul>
+          </div>
 
-        <div class="col-md-3 col-sm-6 footer-section">
-          <h5 class="fw-bold">Follow Us</h5>
-          <ul class="list-unstyled">
-            <li><a href="#" class="text-white text-decoration-none">Instagram</a></li>
-            <li><a href="#" class="text-white text-decoration-none">TikTok</a></li>
-            <li><a href="#" class="text-white text-decoration-none">Twitter</a></li>
-            <li><a href="#" class="text-white text-decoration-none">YouTube</a></li>
-          </ul>
-        </div>
+          <div class="col-md-3 col-sm-6 footer-section">
+            <h5 class="fw-bold">Follow Us</h5>
+            <ul class="list-unstyled">
+              <li><a href="#" class="text-white text-decoration-none">Instagram</a></li>
+              <li><a href="#" class="text-white text-decoration-none">TikTok</a></li>
+              <li><a href="#" class="text-white text-decoration-none">Twitter</a></li>
+              <li><a href="#" class="text-white text-decoration-none">YouTube</a></li>
+            </ul>
+          </div>
 
-      </div>
+        </div>
+      </footer>
     </div>
-  </footer>
+  </div>
 </template>
 
 <script>
@@ -53,6 +55,11 @@ export default {
 </script>
 
 <style scoped>
+.footer-container {
+  background-color: #58cc02; /* Moved background color here so it wraps everything */
+  padding: 20px 0;
+}
+
 .footer-section {
   margin-bottom: 20px;
 }
